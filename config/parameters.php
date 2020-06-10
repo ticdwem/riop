4b79f333d5f6741455fb1215ee0acd604c2a6768
mike121986 <maft_1986@hotmail.com>
2020-06-09 20:20:26 -0500

    agregamos la ruta de la pagina donde se encuentran las imagens que se alojan en la base de datos

--- config/parameters.php
+++ config/parameters.php
@@ -1,3 +1,4 @@
 <?php
 
 define("base_url", "http://localhost/final-catalogo/"); // base url para llamadas absolutas de la url
+define("url_foranea", "http://ticdwem.com/ferreterariopisuena/"); // base url para llamadas absolutas de la url
