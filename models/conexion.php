a32f1b9297b24e8a7bdbd7cf3fa828e9c2a5a1e4
mike121986 <maft_1986@hotmail.com>
2020-06-13 15:07:50 -0500

    cambiamos la clnexion a una local, se recomienda comentar las lineas de conexion local a conexion remota

--- models/conexion.php
+++ models/conexion.php
@@ -5,10 +5,12 @@ class Conexion{
 	public function conectar(){
 
 		try {
-			$link = new PDO("mysql:host=107.180.51.82;dbname=rioPisuena","riop","TiCDWem_1986", array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES  \'UTF8\''));
-			
+			// $link = new PDO("mysql:host=107.180.51.82;dbname=rioPisuena","riop","TiCDWem_1986", array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES  \'UTF8\''));
+
+			$link = new PDO("mysql:host=localhost;dbname=riopisuena","root","", array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES  \'UTF8\''));
+		return $link;
 		} catch (Exception $e) {
-			die("error Conexting to Mysql Server:". $exc->getMessage());
+			die("error Conexting to Mysql Server:". $e->getMessage());
 		}
 
 		return $link;
