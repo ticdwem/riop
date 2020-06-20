56433c00d1c17a7fa984560d22ddd3323daeec61
mike121986 <maft_1986@hotmail.com>
2020-06-19 21:42:06 -0500

    validaciones para enviar el pdf

--- controllers/CatalogoController.php
+++ controllers/CatalogoController.php
@@ -1,6 +1,8 @@
 <?php
 
 
+use Spipu\Html2Pdf\Html2Pdf;
+
 class CatalogoController{
     private $idP;
 
@@ -18,32 +20,41 @@ class CatalogoController{
 			}
 		}
         if(isset($_SESSION['carrito'])){
+            $fullCart = count($_SESSION['carrito']);
             $counter = 0;
            foreach ($_SESSION['carrito'] as $indice => $elmento) {
                if($elmento['idPRoducto'] == $producto_id){
-                    $_SESSION['carrito'][$indice]['unidades']++;
-                    $counter ++;
+                    // $_SESSION['carrito'][$indice]['unidades']++;
+                    // $counter ++;
+                    echo 4;
+                    exit();
                }
            }
         }
         if(!isset($counter) || $counter == 0){
-            // conseguir producto
-            $addPro = new Carrito();
-            $addPro->setPrivateid($producto_id);
-            $addPro->setPrivatetabla('productos');
-            $addPro->setPrivatetabla2('marca');
-            $addPro->setPrivatetabla3('sublinea');
-            $addPro = $addPro->getProductSpecifict();
-            if(is_array($addPro)){
-                $_SESSION['carrito'][]=array(
-                    "idPRoducto"=>$addPro[0]['codigoProducto'],
-                    "sku"=>$addPro[0]['skuProducto'],
-                    "precio" => $addPro[0]['precio'],
-                    "unidades"=>1,
-                    "producto"=>$addPro
-                );
+            if(!isset($_SESSION['carrito'])||$fullCart<10){ 
+                // conseguir producto
+                $addPro = new Carrito();
+                $addPro->setPrivateid($producto_id);
+                $addPro->setPrivatetabla('productos');
+                $addPro->setPrivatetabla2('marca');
+                $addPro->setPrivatetabla3('sublinea');
+                $addPro = $addPro->getProductSpecifict();
+                if(is_array($addPro)){
+                    $_SESSION['carrito'][]=array(
+                        "idPRoducto"=>$addPro[0]['codigoProducto'],
+                        "sku"=>$addPro[0]['skuProducto'],
+                        "precio" => $addPro[0]['precio'],
+                        "unidades"=>1,
+                        "producto"=>$addPro
+                    );
+                }else{
+                    echo 0;
+                    exit();
+                }
             }else{
-                echo 0;
+                echo 3;
+                exit();
             }
         }
         echo 1;
@@ -57,6 +68,19 @@ class CatalogoController{
     public function delete_all(){
         require_once 'helpers/utils.php';
         Utls::deleteSession('carrito');
+
+    }
+
+    public function generatePdf($datos){
+require_once "../../vendor/autoload.php";
+
+
+$html2pdf = new Html2Pdf();
+$html = '<h1>HelloWorld</h1>This is my first test';
+$html.="<p>este es unmaster en php</p>";
+$html2pdf->writeHTML($html);
+$html2pdf->output('juihihi.pdf');
+
 
     }
 }
