35c6840907fefb7c2b65dcdb70cf9dc734b00c29
mike121986 <maft_1986@hotmail.com>
2020-06-25 14:15:34 -0500

    agregamos constatnte para obtener el el usuario de windows

--- config/parameters.php
+++ config/parameters.php
@@ -2,3 +2,4 @@
 
 define("base_url", "http://localhost/final-catalogo/"); // base url para llamadas absolutas de la url
 define("url_foranea", "http://ticdwem.com/ferreterariopisuena/"); // base url para llamadas absolutas de la url
+define("url_home", getenv("HOMEDRIVE") . getenv("HOMEPATH")); // base url para llamadas absolutas de la url
