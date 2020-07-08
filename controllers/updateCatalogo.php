a04b73fd23ccc14a27fd9510705de4f5a8d0a43b
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-07-07 19:22:44 -0500

    se guarda este archivo posteriormente se modificara

--- controllers/updateCatalogo.php
+++ controllers/updateCatalogo.php
@@ -1,19 +1,22 @@
 <?php 
+
 use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
 use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
 use Box\Spout\Writer\Common\Creator\Style\StyleBuilder;
 use Box\Spout\Common\Entity\Style\CellAlignment;
 use Box\Spout\Common\Entity\Style\Color;
-class datosBack
+class datosBackIn
 {
 
 	public function readexcel($argument){
-	
+	echo "*/*/*/*/**/*/*/*/*/*/*/*/*/*";
+	var_dump($argument);
+	exit();
 		if (!empty($_FILES['fileContacts']['name'])) {
 			$archivo = $_FILES["fileContacts"]["name"];
 			$archivoCopiado = $_FILES["fileContacts"]["tmp_name"];
-			$archivo_guardado =  url_home."\AppData\Local\Temp".$archivo;
-			// $archivo_guardado = "../../guardarExcel/copia_".$archivo;
+			// $archivo_guardado =  url_home."\AppData\Local\Temp".$archivo;
+			$archivo_guardado = "../../guardarExcel/copia_".$archivo;
 			$copi = copy($archivoCopiado,$archivo_guardado );
 
 			$reader = ReaderEntityFactory::createXLSXReader();
@@ -45,82 +48,11 @@ class datosBack
 			echo 2;
 		}
 	}
-	
-	public function gnerateQuery($argument)
-	{
-	require_once "../../vendor/autoload.php";
-		$categoria;
-		$tabla;
-		$data = json_decode($_POST['chek'],true);
-		$last = end($data);
-		$contar = count($data);
-		unset($data[$contar-1]);
-
-		$table = new Validacion();
-		$id = $table->validarNumero($last['id']);
-		$tbl = $table->pregmatchletras($last['tabla']);
-		if($id != 0 || $tbl != 0){
-			for ($i=0; $i <$contar-1 ; $i++) { 
-				$dato = $table->pregmatchletras($data[$i]);
-				if($dato == '0'){
-					echo '100';
-					break;
-				}
-			}
-
-			$datos = implode(",", $data);
-			switch ($last['tabla']) {
-				case 'marca':
-					$categoria = "idMarcaProdcuto";
-					$tabla = "productos";
-					break;
-				case 'sublinea':
-					$categoria = "idSublineaProducto";
-					$tabla = "productos";
-					break;	
-				case 'proveedor':
-					$categoria = "idProveedorProducto";
-					$tabla = "productos";
-					break;
-				case 'linea':
-					$categoria = "linea.idLinea";
-					$tabla = "productos 
-								INNER JOIN sublinea 
-								ON idSublineaProducto = idSublinea 
-								INNER JOIN linea 
-								ON sublinea.idLinea = linea.idLinea";
-					break;				
-				default:
-					$categoria = "idMarcaProdcuto";
-					$tabla = "productos";
-					break;
-			};
-
-			$get = new GetAndSetFilas();
-			$get ->setDatos($datos);
-			$get ->setTabla($tabla);
-			$get ->setWhere($categoria);
-			$get ->setIdCategoria($last['id']);
-			$filas = $get->getDinamico();
-			
-			$writer = WriterEntityFactory::createXLSXWriter();
-			$filePath = url_home."\Downloads\modificar".ucfirst($last['tabla']).date('i_s').".xlsx";
-			$writer->openToFile($filePath);
-			//agregamos encabezado
-			$row = WriterEntityFactory::createRowFromArray($data);
-			$writer->addRow($row);
-			//imprimimos en las filas
-			 foreach ($filas as $indice) {
-				$row = WriterEntityFactory::createRowFromArray($indice);
-				$writer->addRow($row);
-			 }
-			$writer->close();
-			echo '1';
-		}else{
-			echo '100';
-		}
-
-		
-		
-	}
 }
+
+// if(isset($_GET["upDatos"])){
+// 	echo "*/*/*//*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/";
+// 	$archivo = $_FILES["fileContacts"]["name"];
+// 	var_dump($_GET["upDatos"]);
+// 	var_dump($_GET["upDatos"]);
+// }
