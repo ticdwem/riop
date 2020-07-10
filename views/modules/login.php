ee19b699b3c343d03a307009af766c6416f76c10
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-07-10 17:06:53 -0500

    hacemos una vista de loguin para el usuario

---
+++ views/modules/login.php
@@ -0,0 +1,30 @@
+<div class="main">
+   <div class="contenedorLoguin">
+      <center>
+         <div class="middle">
+            <div id="login">
+               <form>
+                  <fieldset class="clearfix">
+                     <p ><span class="fa fa-user"></span><input type="text" id="user"  Placeholder="Usuario" required></p> 
+                     <p><span class="fa fa-lock"></span><input type="password" id="pass"  Placeholder="Contraseña" required></p>
+                     <div>
+                        <span style="width:48%; text-align:left;  display: inline-block;"><a class="small-text" href="#"></a></span>
+                        <span style="width:50%; text-align:right;  display: inline-block;"><input type="submit" id="sigin" value="Sign In"></span>
+                     </div>
+                  </fieldset>
+                  <div class="clearfix"></div>
+               </form>
+               <div class="clearfix"></div>
+            </div> <!-- end login -->
+            <div class="logo">
+               <figure>
+                  <figcaption>
+                     <h2>INICIAR SESSION</h2>
+                  </figcaption>
+                  <img src="<?=base_url?>images/logo.png" alt="logotipo rio pisueña">
+               </figure>
+            </div>
+         </div>
+      </center>
+   </div>
+</div>
