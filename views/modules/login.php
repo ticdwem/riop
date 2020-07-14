247e60da34e49b692d8c1273d0021db0d9bd579d
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-07-13 21:56:24 -0500

    cambiamos etiquetas para los mensaje que provienen de jquery

--- views/modules/login.php
+++ views/modules/login.php
@@ -6,15 +6,16 @@
                <form>
                   <fieldset class="clearfix">
                      <p ><span class="fa fa-user"></span><input type="text" id="user"  Placeholder="Usuario" required></p> 
-                     <p><span class="fa fa-lock"></span><input type="password" id="pass"  Placeholder="Contraseña" required></p>
+                     <!-- <p><span class="fa fa-lock"></span><input type="password" id="pass"  Placeholder="Contraseña" required></p> -->
+                     <p><span class="fa fa-lock"></span><input type="password" id="pass" class="form-control" class="input-block-level" data-toggle="tooltip" title="MAYUS ACTIVADA" data-trigger="manual" data-title="La tecla Bloq Mayús está activada" placeholder="Contraseña" autocomplete="off" required></p>
                      <div>
                         <span style="width:48%; text-align:left;  display: inline-block;"><a class="small-text" href="#"></a></span>
                         <span style="width:50%; text-align:right;  display: inline-block;"><input type="submit" id="sigin" value="Sign In"></span>
                      </div>
                   </fieldset>
-                  <div class="clearfix"></div>
+                  <div class="clearfix error_validacion"></div>
                </form>
-               <div class="clearfix"></div>
+               <div class="clearfix errorVacio"></div>
             </div> <!-- end login -->
             <div class="logo">
                <figure>
