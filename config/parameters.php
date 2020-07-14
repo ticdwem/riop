1c342408564d662cc329f4280264a39c466154ab
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-07-13 22:26:21 -0500

    este archivo debe cabiarse cuando se seuba el servidor real

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,6 @@
 <?php
 
-define("base_url", "http://192.168.1.69/final-catalogo/"); // base url para llamadas absolutas de la url
+define("base_url", "http://localhost/final-catalogo/"); // base url para llamadas absolutas de la url
+// define("base_url", "http://192.168.1.69/final-catalogo/"); // base url para llamadas absolutas de la url
 define("url_foranea", "http://ticdwem.com/ferreterariopisuena/"); // base url para llamadas absolutas de la url
 define("url_home", getenv("HOMEDRIVE") . getenv("HOMEPATH")); // base url para llamadas absolutas de la url
