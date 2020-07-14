a3d22c20f3e2c517a97d2e1cfcff14d5e4785630
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-07-13 22:22:42 -0500

    creamos pagina de error 404, insertamos una imagen de error404

---
+++ views/modules/error404.php
@@ -0,0 +1,12 @@
+<?php include "views/modules/header.php"; ?>
+<div class="error">
+	<figure>
+		<figcaption>
+		 <h2>RIO PISUEÑA</h2>
+		</figcaption>
+	<img src="<?=base_url?>images/error404.jpg" alt="https://www.freepik.es/fotos-vectores-gratis/negocios  Vector de Negocios creado por pikisuperstar - www.freepik.es">
+	</figure>
+</div>
+<?php
+include "views/modules/footer.php";
+?>
