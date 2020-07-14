040569e5fd95d4c3b5cdd0c30b6bdfee74804d4b
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-07-13 22:09:43 -0500

    agregamos funcion para verificar el inicio de session del usuario, cambiamos, agregamos quote para escarpar los datos raros ingresados por html, y agregamos encriptacion hash al password

--- models/usuario.php
+++ models/usuario.php
@@ -19,7 +19,7 @@
 	} 
 
 	function setNombre($nombre) {  
-		$this->nombre = $nombre; 
+		$this->nombre = $this->db->quote($nombre); 
 	} 
 
 	function getApellidos() { 
@@ -27,7 +27,7 @@
 	} 
 
 	function setApellidos($apellidos) {  
-		$this->apellidos = $apellidos; 
+		$this->apellidos = $this->db->quote($apellidos); 
 	} 
 
 	function getNombreUsuario() { 
@@ -35,15 +35,16 @@
 	} 
 
 	function setNombreUsuario($nombreUsuario) {  
-		$this->nombreUsuario = $nombreUsuario; 
+		$this->nombreUsuario = $this->db->quote($nombreUsuario); 
 	} 
 
 	function getPassword() { 
- 		return $this->password; 
+ 		return password_hash($this->password, PASSWORD_BCRYPT,['cost' => 4]); 
 	} 
 
 	function setPassword($password) {  
-		$this->password = $password; 
+		// $this->password = password_hash($this->db->quote($password), PASSWORD_DEFAULT); 
+		$this->password = $password;
 	} 
 
 	function getEmail() { 
@@ -51,21 +52,21 @@
 	} 
 
 	function setEmail($email) {  
-		$this->email = $email; 
-	} 
+		$this->email = $this->db->quote($email); 
+	}  
 
 	function getTipoUser() { 
  		return $this->tipoUser; 
 	} 
 
 	function setTipoUser($tipoUser) {  
-		$this->tipoUser = $tipoUser; 
+		$this->tipoUser = $this->db->quote($tipoUser); 
     } 
 
     public function insertUser(){
         $insert = "INSERT INTO usuarios
         (nombreUsuario, apellidosUsuario, nobreUsuario, passwordUsuario, emailUsuario, tipoUsuario, statusUsuario) 
-        VALUES ('{$this->getNombre()}', '{$this->getApellidos()}', '{$this->getNombreUsuario()}', '{$this->getPassword()}', '{$this->getEmail()}', '{$this->getTipoUser()}', '1')";
+        VALUES ({$this->getNombre()}, {$this->getApellidos()}, {$this->getNombreUsuario()}, '{$this->getPassword()}', {$this->getEmail()}, {$this->getTipoUser()}, '1')";
         $user = $this->db->prepare($insert);
         $user->execute();
         if($user){
@@ -75,5 +76,28 @@
         }
         $user->close();
 
+    }
+
+    public function inSistema(){
+    	$result = false;
+    	$passwd = $this->password;
+    	$user = "SELECT * FROM usuarios WHERE nobreUsuario = {$this->getNombreUsuario()}";
+    	$datos = $this->db->prepare($user);
+    	$datos->execute();
+    	$registro = $datos->fetchAll(PDO::FETCH_OBJ);
+    	 if(!empty($registro)){
+    	 	$usuario = $registro[0];
+    	 	$veryPass = password_verify($passwd, $usuario->passwordUsuario);
+    	 	if($veryPass){
+    	 		return $usuario;    	 		
+    	 	}else{
+    	 		return 0;
+    	 	}
+    	 	
+    	 }else{
+    	 	return 2;
+    	 }
+
+    // $datos->close();
     }
  }
